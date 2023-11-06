cef62b7178ddc83959da5a45419d4569cfe629af
prajwal maharjan <HeyAbecedarian>
2023-11-06 13:29:34 +0545

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'> 
+      <div className="descriptionbox-navigator">
+        <div className='descriptionbox-nav-box'>Description</div>
+        <div className='descriptionbox-nav-box fade'>Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p> E-commerce (electronic commerce) is the buying and selling of goods and services, or the transmitting of funds or data, over an electronic network, primarily the internet. These business transactions occur either as business-to-business (B2B), business-to-consumer (B2C), consumer-to-consumer or consumer-to-business.
+
+The terms e-commerce and e-business are often used interchangeably. The term e-tail is also sometimes used in reference to the transactional processes that make up online retail shopping.</p>
+        <p> Ecommerce is a method of buying and selling goods and services online. The definition of ecommerce business can also include tactics like affiliate marketing. You can use ecommerce channels such as your own website, an established selling website like Amazon, or social media to drive online sales.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
---
+++ src/Pages/LoginSignup.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './CSS/LoginSignup.css'
+
+const LoginSignup = () => {
+  return (
+    <div className='loginsignup'>
+      <div className='loginsignup-container'>
+        <h1>Sign Up</h1>
+      </div>
+      <div className='loginsignup-fields'>
+        <input type='text' placeholder='your name'/>
+        <input type='email' placeholder='email address'/>
+        <input type='password' placeholder='password'/>
+      </div>
+      <button>Continue</button>
+      <p className='loginsignup-login'>Already have an account?<span>Login here</span></p>
+      <div className='loginsignup-agree'>
+        <input type='checkbox' name='' id=''/>
+        <p>By continuing, I agree to th terms of use & privacy policy.</p>
+
+      </div>
+    </div>
+  )
+}
+
+export default LoginSignup
